82cf29a2e48330bea3e01efad98f9a9c86ffcaba
meetmangukiya2710 <150114652+meetmangukiya2710@users.noreply.github.com>
2024-05-23 12:06:21 +0530

    Error Solve

--- Builder.cpp
+++ Builder.cpp
@@ -1,144 +1,80 @@
 #include <iostream>
-#include <string>
-
-/* Car parts */
-class Wheel
-{
-    public:
-        int size;
+using namespace std;
+ 
+class Computer {
+public:
+    void setCPU(const string& cpu) {
+        cpu_ = cpu;
+    }
+ 
+    void setRAM(const string& ram) {
+        ram_ = ram;
+    }
+ 
+    void setStorage(const string& storage) {
+        storage_ = storage;
+    }
+ 
+    void displayInfo() const {
+        cout << "Computer Configuration:"
+                  << "\nCPU: " << cpu_
+                  << "\nRAM: " << ram_
+                  << "\nStorage: " << storage_ << "\n\n";
+    }
+ 
+private:
+    string cpu_;
+    string ram_;
+    string storage_;
 };
-
-class Engine
-{
-    public:
-        int horsepower;
+ 
+class Builder {
+public:
+    virtual void buildCPU() = 0;
+    virtual void buildRAM() = 0;
+    virtual void buildStorage() = 0;
+    virtual Computer getResult() = 0;
 };
-
-class Body
-{
-    public:
-        std::string shape;
+ 
+class GamingComputerBuilder : public Builder {
+private:
+    Computer computer_;
+ 
+public:
+    void buildCPU() {
+        computer_.setCPU("Gaming CPU");
+    }
+ 
+    void buildRAM() {
+        computer_.setRAM("16GB DDR4");
+    }
+ 
+    void buildStorage() {
+        computer_.setStorage("1TB SSD");
+    }
+ 
+    Computer getResult() {
+        return computer_;
+    }
 };
-class Car
-{
-    public:
-        Wheel*   wheels[4];
-        Engine*  engine;
-        Body* body;
-    
-        void specifications()
-        {
-            std::cout << "body:" << body->shape << std::endl;
-            std::cout << "engine horsepower:" << engine->horsepower << std::endl;
-            std::cout << "tire size:" << wheels[0]->size << "'" << std::endl;
-        }
+ 
+class ComputerDirector {
+public:
+    void construct(Builder& builder) {
+        builder.buildCPU();
+        builder.buildRAM();
+        builder.buildStorage();
+    }
 };
-
-class Builder
-{
-    public:
-        virtual Wheel* getWheel() = 0;
-        virtual Engine* getEngine() = 0;
-        virtual Body* getBody() = 0;
-};
-
-class Director
-{
-    Builder* builder;
-
-    public:
-        void setBuilder(Builder* newBuilder)
-        {
-            builder = newBuilder;
-        }
-
-        Car* getCar()
-        {
-            Car* car = new Car();
-
-            car->body = builder->getBody();
-
-            car->engine = builder->getEngine();
-
-            car->wheels[0] = builder->getWheel();
-            car->wheels[1] = builder->getWheel();
-            car->wheels[2] = builder->getWheel();
-            car->wheels[3] = builder->getWheel();
-
-            return car;
-        }
-};
-
-class JeepBuilder : public Builder
-{
-    public:
-        Wheel* getWheel()
-        {
-            Wheel* wheel = new Wheel();
-            wheel->size = 22;
-            return wheel;
-        }
-
-        Engine* getEngine()
-        {
-            Engine* engine = new Engine();
-            engine->horsepower = 400;
-            return engine;
-        }
-
-        Body* getBody()
-        {
-            Body* body = new Body();
-            body->shape = "SUV";
-        }
-};
-
-class NissanBuilder : public Builder
-{
-    public:
-        Wheel* getWheel()
-        {
-            Wheel* wheel = new Wheel();
-            wheel->size = 16;
-            return wheel;
-        }
-
-        Engine* getEngine()
-        {
-            Engine* engine = new Engine();
-            engine->horsepower = 85;
-            return engine;
-        }
-
-        Body* getBody()
-        {
-            Body* body = new Body();
-            body->shape = "hatchback";
-        }
-};
-
-
-int main()
-{
-    Car* car;
-
-    Director director;
-
-    JeepBuilder jeepBuilder;
-    NissanBuilder nissanBuilder;
-
-    std::cout << "Jeep" << std::endl;
-    director.setBuilder(&jeepBuilder);
-    car = director.getCar();
-    car->specifications();
-
-    std::cout << std::endl;
-
-    /* Build a Nissan */
-    std::cout << "Nissan" << std::endl;
-    director.setBuilder(&nissanBuilder); 
-    car = director.getCar();
-    car->specifications();
-
+ 
+int main() {
+    GamingComputerBuilder gamingBuilder;
+    ComputerDirector director;
+ 
+    director.construct(gamingBuilder);
+    Computer gamingComputer = gamingBuilder.getResult();
+ 
+    gamingComputer.displayInfo();
+ 
     return 0;
 }
